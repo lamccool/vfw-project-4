6bbbebc6adee2d258cea34912b5aee9f8bbd9286
Laura McCool <mccool_laura@yahoo.com>
2012-01-26 17:31:53 -0600

    added files to folder as well as created the json objects

---
+++ json.js
@@ -0,0 +1,30 @@
+var json = {
+	"gift1": {
+		"category":["Category:", "Electronics"], 
+		"comments":["Gift Description:", "Digital Camera"],
+		"amount":["Quantity:","1"],
+		"location":["Where To Buy:","Online"],
+		"store":["Store Name:","Amazon"],
+		"url":["Product Url:","amazon.com"],
+		"date":["Date Added:","2011-12-01"]
+		},
+	"gift2": {
+		"category":["Category:", "Movies & Games"], 
+		"comments":["Gift Description:", "Super 8 Bluray"],
+		"amount":["Quantity:","1"],
+		"location":["Where To Buy:","Online"],
+		"store":["Store Name:","Amazon"],
+		"url":["Product Url:","amazon.com"],
+		"date":["Date Added:","2011-11-27"]
+		},
+	"gift3": {
+		"category":["Category:", "Clothing & Accessories"], 
+		"comments":["Gift Description:", "Skinny Jeans"],
+		"amount":["Quantity:","2"],
+		"location":["Where To Buy:","Online"],
+		"store":["Store Name:","Delias"],
+		"url":["Product Url:","delias.com"],
+		"date":["Date Added:","2011-11-30"]
+		}
+	}
+
